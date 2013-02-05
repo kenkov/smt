3bf2ff4b8ab2c6594d160e136ef74f43a51e57d3
Noriyuki ABE <kenko.py@gmail.com>
2013-02-05 11:33:55 -0800

    modifying ibmmodel1 by Clojure

--- ibmmodel1.clj
+++ ibmmodel1.clj
@@ -1,6 +1,6 @@
 (require '[clojure.string :as st])
 
-(defn _split-space
+(defn split-space
   "split at spaces"
   [sentence]
   (st/split sentence #"\s"))
@@ -8,7 +8,7 @@
 (defn sents-to-corpus
   [sents]
   (for [[es fs] sents]
-    [(_split-space es) (_split-space fs)]))
+    [(split-space es) (split-space fs)]))
 
 (defn corpus
   "convert from sentences to corpus"
@@ -20,11 +20,45 @@
   (set
     (reduce into (for [[_ fs] corpus] fs))))
 
+(defn sum
+  [lst]
+  (reduce + 0 lst))
+
+(defn count-keys
+  [ky lst]
+  (sum (map #(if (= % ky) 1 0) lst)))
+
+(defn count-function
+  [e f es fs t]
+  (let
+    [e-count (count-keys e es)
+     f-count (count-keys f fs)
+     t-sum (sum (for [_f fs] (t [e _f])))]
+    (* (/ (t [e f]) t-sum) (* e-count f-count))))
+
+(defn exist
+  [ky lst]
+  (some #{ky} lst))
+
+;(defn count-dic
+;  [corpus]
+;  "define this function first"
+;  (let [dic {}]
+;    (for [[es fs] corpus]
+;      (for [e es, f fs]
+;        [into dic
+;
+;)
+
+;(defn update
+;  [e f t corpus]
+;  (let
+;    [numerator (sum (for [[es fs] corpus
+;                          :when (and (exist e es)
+;                                     (exist f fs))]
+;                      (count-function e f es fs t)))
+;     denominator
+
 (def sents [["the house", "das Haus"]
             ["the book", "das Buch"]
             ["a book", "ein Buch"]])
-
-
-
-
-
