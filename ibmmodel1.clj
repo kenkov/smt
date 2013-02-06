ed9c6c17e8b6c4671c10ea5e1c22db59d59294bd
Noriyuki ABE <kenko.py@gmail.com>
2013-02-06 06:30:23 -0800

    implements count, total, and estimate functions

--- ibmmodel1.clj
+++ ibmmodel1.clj
@@ -15,11 +15,6 @@
   [sents]
   (sents-to-corpus sents))
 
-(defn f-words
-  [corpus]
-  (set
-    (reduce into (for [[_ fs] corpus] fs))))
-
 (defn sum
   [lst]
   (reduce + 0 lst))
@@ -36,28 +31,54 @@
      t-sum (sum (for [_f fs] (t [e _f])))]
     (* (/ (t [e f]) t-sum) (* e-count f-count))))
 
-(defn exist
-  [ky lst]
-  (some #{ky} lst))
+(defn s-total
+  [es fs t]
+  (let
+    [foreign-es (into [] (set es))]
+    (let [mp (into [] (map #(vector % (t %)) (for [e foreign-es f fs] [e f])))]
+      (reduce
+        (fn [_mp [[e f] vl]] (conj _mp [e (+ (_mp e 0) vl)]))
+        {}
+        mp))))
 
-;(defn count-dic
-;  [corpus]
-;  "define this function first"
-;  (let [dic {}]
-;    (for [[es fs] corpus]
-;      (for [e es, f fs]
-;        [into dic
-;
-;)
+(defn count-func
+  [es fs t s-total]
+  (let [mp (into [] (map #(vector % (t %)) (for [e es f fs] [e f])))]
+    (reduce
+      (fn [_mp [[e f] vl]]
+        (conj _mp [[e f] (+ (_mp [e f] 0)
+                            (/ (t [e f]) (s-total e)))]))
+      {}
+      mp)))
 
-;(defn update
-;  [e f t corpus]
-;  (let
-;    [numerator (sum (for [[es fs] corpus
-;                          :when (and (exist e es)
-;                                     (exist f fs))]
-;                      (count-function e f es fs t)))
-;     denominator
+(defn total-func
+  [es fs t s-total]
+  (let [mp (into [] (map #(vector % (t %)) (for [e es f fs] [e f])))]
+    (reduce
+      (fn [_mp [[e f] vl]]
+        (conj _mp [f (+ (_mp f 0)
+                        (/ (t [e f]) (s-total e)))]))
+      {}
+      mp)))
+
+(defn estimate
+  [count-map total-map]
+  (reduce
+    (fn [_mp [[e f] vl]]
+      (conj _mp [[e f] (/ (count-map [e f]) (total-map f))]))
+    {}
+    count-map))
+
+; tests
+(def es [1 2 1])
+(def fs [4 5 4])
+(def t {[1 4] 1, [1 5] 2, [2 4] 3, [2 5] 4})
+(def stotal (s-total es fs t))
+
+(s-total es fs t)
+(def count-map (count-func es fs t stotal))
+(def total-map (total-func es fs t stotal))
+(estimate count-map total-map)
 
 (def sents [["the house", "das Haus"]
             ["the book", "das Buch"]
