5deed6c73f0e1357eeecd2f59f9f114fa7ebac2f
Noriyuki Abe <kenko.py@gmail.com>
2013-03-08 13:47:40 -0800

    revise testcode using with

--- test/test_stackdecoder.py
+++ test/test_stackdecoder.py
@@ -14,29 +14,43 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
+class DBSetup(object):
+
+    def __init__(self, db="sqlite:///:memory:"):
+        self.db = db
+        self.tables = [TransPhraseProb, Phrase]
+
+    def __enter__(self):
+        self.engine = create_engine(self.db)
+        # create tables
+        for Table in self.tables:
+            Table.__table__.create(self.engine)
+
+        # create session
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # drop table
+        for Table in self.tables:
+            Table.__table__.drop(self.engine, checkfirst=True)
+        self.session.close()
+
+
 class FutureCostEstimateTest(unittest.TestCase):
 
     def setUp(self):
         self.maxDiff = None
 
-    def test_future_cost_estimate(self):
+    def test_future_cost_estimate_2to1(self):
         sentences = u"the tourism initiative addresses this\
             for the first time".split()
         transfrom = 2
         transto = 1
         init_val = 100.0
         db = "sqlite:///test/:test:"
-
-        engine = create_engine(db)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        # drop table
-        TransPhraseProb.__table__.drop(engine, checkfirst=True)
-        Phrase.__table__.drop(engine, checkfirst=True)
-        # create table
-        TransPhraseProb.__table__.create(engine)
-        Phrase.__table__.create(engine)
 
         # data set
         dataset = [("1", "the", Frac(-1), 0),
@@ -93,21 +107,6 @@ class FutureCostEstimateTest(unittest.TestCase):
                    ("1", "for the first time", Frac(-23, 10), 0),
                    ("1", "for the first time", Frac(-33, 10), 0),
                    ]
-        session.add_all(TransPhraseProb(lang1p=item[0],
-                                        lang2p=item[1],
-                                        p2_1=item[2],
-                                        p1_2=item[3])
-                        for item in dataset)
-        session.add_all(Phrase(lang1p=item[0],
-                               lang2p=item[1])
-                        for item in dataset)
-        session.commit()
-
-        ans = future_cost_estimate(sentences,
-                                   transfrom=transfrom,
-                                   transto=transto,
-                                   init_val=init_val,
-                                   db=db)
         val = {(1, 1): -1.0,
                (1, 2): -3.0,
                (1, 3): -4.5,
@@ -155,14 +154,287 @@ class FutureCostEstimateTest(unittest.TestCase):
                (9, 9): -1.6,
                }
 
-        # drop table
-        TransPhraseProb.__table__.drop(engine, checkfirst=True)
-        Phrase.__table__.drop(engine, checkfirst=True)
+        with DBSetup(db) as dbobj:
+            dbobj.session.add_all(TransPhraseProb(lang1p=item[0],
+                                                  lang2p=item[1],
+                                                  p2_1=item[2],
+                                                  p1_2=item[3])
+                                  for item in dataset)
+            dbobj.session.add_all(Phrase(lang1p=item[0],
+                                         lang2p=item[1])
+                                  for item in dataset)
+            dbobj.session.commit()
+
+            ans = future_cost_estimate(sentences,
+                                       transfrom=transfrom,
+                                       transto=transto,
+                                       init_val=init_val,
+                                       db=db)
+        # assert
+        self.assertEqual(ans, val)
+
+    def test_future_cost_estimate_2to1_argument_not_satisfied(self):
+        sentences = u"the tourism initiative addresses this\
+            for the first time".split()
+        transfrom = 2
+        transto = 1
+        init_val = 100.0
+        db = "sqlite:///test/:test:"
+
+        # data set
+        dataset = [("1", "the", Frac(-1), 0),
+                   ("1", "the", Frac(-2), 0),
+                   # 2
+                   ("1", "tourism", Frac(-2), 0),
+                   ("1", "tourism", Frac(-3), 0),
+                   # 3
+                   ("1", "initiative", Frac(-15, 10), 0),
+                   ("1", "initiative", Frac(-25, 10), 0),
+                   # 4
+                   ("1", "addresses", Frac(-24, 10), 0),
+                   ("1", "addresses", Frac(-34, 10), 0),
+                   # 5
+                   #("1", "this", Frac(-14, 10), 0),
+                   #("1", "this", Frac(-24, 10), 0),
+                   # 6
+                   ("1", "for", Frac(-1), 0),
+                   ("1", "for", Frac(-2), 0),
+                   # 7
+                   ("1", "the", Frac(-1), 0),
+                   ("1", "the", Frac(-2), 0),
+                   # 8
+                   ("1", "first", Frac(-19, 10), 0),
+                   ("1", "first", Frac(-29, 10), 0),
+                   # 9
+                   ("1", "time", Frac(-16, 10), 0),
+                   ("1", "time", Frac(-26, 10), 0),
+                   # 10
+                   ("1", "initiative addresses", Frac(-4), 0),
+                   ("1", "initiative addresses", Frac(-4), 0),
+                   # 11
+                   ("1", "this for", Frac(-25, 10), 0),
+                   ("1", "this for", Frac(-35, 10), 0),
+                   # 12
+                   ("1", "the first", Frac(-22, 10), 0),
+                   ("1", "the first", Frac(-32, 10), 0),
+                   # 13
+                   ("1", "for the", Frac(-13, 10), 0),
+                   ("1", "for the", Frac(-23, 10), 0),
+                   # 14
+                   ("1", "first time", Frac(-24, 10), 0),
+                   ("1", "first time", Frac(-34, 10), 0),
+                   # 15
+                   ("1", "this for the", Frac(-27, 10), 0),
+                   ("1", "this for the", Frac(-37, 10), 0),
+                   # 16
+                   ("1", "for the first", Frac(-23, 10), 0),
+                   ("1", "for the first", Frac(-33, 10), 0),
+                   # 17
+                   ("1", "the first time", Frac(-23, 10), 0),
+                   ("1", "the first time", Frac(-33, 10), 0),
+                   # 18
+                   ("1", "for the first time", Frac(-23, 10), 0),
+                   ("1", "for the first time", Frac(-33, 10), 0),
+                   ]
+
+        val = {(1, 1): -1.0,
+               (1, 2): -3.0,
+               (1, 3): -4.5,
+               (1, 4): -6.9,
+               (1, 5): -106.9,
+               (1, 6): -9.4,
+               (1, 7): -9.6,
+               (1, 8): -11.5,
+               (1, 9): -11.7,
+               (2, 2): -2.0,
+               (2, 3): -3.5,
+               (2, 4): -5.9,
+               (2, 5): -105.9,
+               (2, 6): -8.4,
+               (2, 7): -8.6,
+               (2, 8): -10.5,
+               (2, 9): -10.7,
+               (3, 3): -1.5,
+               (3, 4): -3.9,
+               (3, 5): -103.9,
+               (3, 6): -6.4,
+               (3, 7): -6.6,
+               (3, 8): -8.5,
+               (3, 9): -8.7,
+               (4, 4): -2.4,
+               (4, 5): -102.4,
+               (4, 6): -4.9,
+               (4, 7): -5.1,
+               (4, 8): -7.0,
+               (4, 9): -7.199999999999999,
+               (5, 5): -100.0,
+               (5, 6): -2.5,
+               (5, 7): -2.7,
+               (5, 8): -4.6,
+               (5, 9): -4.8,
+               (6, 6): -1.0,
+               (6, 7): -1.3,
+               (6, 8): -2.3,
+               (6, 9): -2.3,
+               (7, 7): -1.0,
+               (7, 8): -2.2,
+               (7, 9): -2.3,
+               (8, 8): -1.9,
+               (8, 9): -2.4,
+               (9, 9): -1.6,
+               }
+
+        with DBSetup(db) as dbobj:
+            dbobj.session.add_all(TransPhraseProb(lang1p=item[0],
+                                                  lang2p=item[1],
+                                                  p2_1=item[2],
+                                                  p1_2=item[3])
+                                  for item in dataset)
+            dbobj.session.add_all(Phrase(lang1p=item[0],
+                                         lang2p=item[1])
+                                  for item in dataset)
+            dbobj.session.commit()
+
+            ans = future_cost_estimate(sentences,
+                                       transfrom=transfrom,
+                                       transto=transto,
+                                       init_val=init_val,
+                                       db=db)
 
         # assert
         self.assertEqual(ans, val)
 
-    def test_text_example(self):
+    def test_future_cost_estimate_1to2(self):
+        sentences = u"the tourism initiative addresses this\
+            for the first time".split()
+        transfrom = 1
+        transto = 2
+        init_val = 100.0
+        db = "sqlite:///test/:test:"
+
+        # data set
+        dataset = [("1", "the", Frac(-1), 0),
+                   ("1", "the", Frac(-2), 0),
+                   # 2
+                   ("1", "tourism", Frac(-2), 0),
+                   ("1", "tourism", Frac(-3), 0),
+                   # 3
+                   ("1", "initiative", Frac(-15, 10), 0),
+                   ("1", "initiative", Frac(-25, 10), 0),
+                   # 4
+                   ("1", "addresses", Frac(-24, 10), 0),
+                   ("1", "addresses", Frac(-34, 10), 0),
+                   # 5
+                   ("1", "this", Frac(-14, 10), 0),
+                   ("1", "this", Frac(-24, 10), 0),
+                   # 6
+                   ("1", "for", Frac(-1), 0),
+                   ("1", "for", Frac(-2), 0),
+                   # 7
+                   ("1", "the", Frac(-1), 0),
+                   ("1", "the", Frac(-2), 0),
+                   # 8
+                   ("1", "first", Frac(-19, 10), 0),
+                   ("1", "first", Frac(-29, 10), 0),
+                   # 9
+                   ("1", "time", Frac(-16, 10), 0),
+                   ("1", "time", Frac(-26, 10), 0),
+                   # 10
+                   ("1", "initiative addresses", Frac(-4), 0),
+                   ("1", "initiative addresses", Frac(-4), 0),
+                   # 11
+                   ("1", "this for", Frac(-25, 10), 0),
+                   ("1", "this for", Frac(-35, 10), 0),
+                   # 12
+                   ("1", "the first", Frac(-22, 10), 0),
+                   ("1", "the first", Frac(-32, 10), 0),
+                   # 13
+                   ("1", "for the", Frac(-13, 10), 0),
+                   ("1", "for the", Frac(-23, 10), 0),
+                   # 14
+                   ("1", "first time", Frac(-24, 10), 0),
+                   ("1", "first time", Frac(-34, 10), 0),
+                   # 15
+                   ("1", "this for the", Frac(-27, 10), 0),
+                   ("1", "this for the", Frac(-37, 10), 0),
+                   # 16
+                   ("1", "for the first", Frac(-23, 10), 0),
+                   ("1", "for the first", Frac(-33, 10), 0),
+                   # 17
+                   ("1", "the first time", Frac(-23, 10), 0),
+                   ("1", "the first time", Frac(-33, 10), 0),
+                   # 18
+                   ("1", "for the first time", Frac(-23, 10), 0),
+                   ("1", "for the first time", Frac(-33, 10), 0),
+                   ]
+
+        val = {(1, 1): -1.0,
+               (1, 2): -3.0,
+               (1, 3): -4.5,
+               (1, 4): -6.9,
+               (1, 5): -8.3,
+               (1, 6): -9.3,
+               (1, 7): -9.6,
+               (1, 8): -10.6,
+               (1, 9): -10.6,
+               (2, 2): -2.0,
+               (2, 3): -3.5,
+               (2, 4): -5.9,
+               (2, 5): -7.3,
+               (2, 6): -8.3,
+               (2, 7): -8.6,
+               (2, 8): -9.6,
+               (2, 9): -9.6,
+               (3, 3): -1.5,
+               (3, 4): -3.9,
+               (3, 5): -5.3,
+               (3, 6): -6.3,
+               (3, 7): -6.6,
+               (3, 8): -7.6,
+               (3, 9): -7.6,
+               (4, 4): -2.4,
+               (4, 5): -3.8,
+               (4, 6): -4.8,
+               (4, 7): -5.1,
+               (4, 8): -6.1,
+               (4, 9): -6.1,
+               (5, 5): -1.4,
+               (5, 6): -2.4,
+               (5, 7): -2.7,
+               (5, 8): -3.6999999999999997,
+               (5, 9): -3.6999999999999997,
+               (6, 6): -1.0,
+               (6, 7): -1.3,
+               (6, 8): -2.3,
+               (6, 9): -2.3,
+               (7, 7): -1.0,
+               (7, 8): -2.2,
+               (7, 9): -2.3,
+               (8, 8): -1.9,
+               (8, 9): -2.4,
+               (9, 9): -1.6,
+               }
+
+        with DBSetup(db) as dbobj:
+            dbobj.session.add_all(TransPhraseProb(lang2p=item[0],
+                                                  lang1p=item[1],
+                                                  p1_2=item[2],
+                                                  p2_1=item[3])
+                                  for item in dataset)
+            dbobj.session.add_all(Phrase(lang1p=item[0],
+                                         lang2p=item[1])
+                                  for item in dataset)
+            dbobj.session.commit()
+
+            ans = future_cost_estimate(sentences,
+                                       transfrom=transfrom,
+                                       transto=transto,
+                                       init_val=init_val,
+                                       db=db)
+        # assert
+        self.assertEqual(ans, val)
+
+    def test__future_cost_estimate(self):
         sentences = u"the tourism initiative addresses this\
             for the first time".split()
         phrase_prob = {(1, 1): Frac(-1),
@@ -184,8 +456,6 @@ class FutureCostEstimateTest(unittest.TestCase):
                        (7, 9): Frac(-23, 10),
                        (6, 9): Frac(-23, 10),
                        }
-        ans = _future_cost_estimate(sentences,
-                                    phrase_prob)
         val = {(1, 1): Frac(-1),
                (1, 2): Frac(-3),
                (1, 3): Frac(-45, 10),
@@ -231,9 +501,11 @@ class FutureCostEstimateTest(unittest.TestCase):
                (8, 8): Frac(-19, 10),
                (8, 9): Frac(-24, 10),
                (9, 9): Frac(-16, 10)}
+        ans = _future_cost_estimate(sentences,
+                                    phrase_prob)
         self.assertEqual(ans, val)
 
-    def test_dict_not_satisfied(self):
+    def test__future_cost_estimate_dict_not_satisfied(self):
         sentences = u"the tourism initiative addresses this\
             for the first time".split()
         phrase_prob = {(1, 1): Frac(-1),
@@ -264,6 +536,7 @@ class FutureCostEstimateTest(unittest.TestCase):
     def test_create_estimate_dict(self):
         sentences = u"the tourism initiative addresses this\
             for the first time".split()
+        init_val = Frac(-100)
         phrase_prob = {(1, 1): Frac(-1),
                        (2, 2): Frac(-2),
                        # lack one value
@@ -285,10 +558,6 @@ class FutureCostEstimateTest(unittest.TestCase):
                        (7, 9): Frac(-23, 10),
                        (6, 9): Frac(-23, 10),
                        }
-        init_val = Frac(-100)
-        ans = _create_estimate_dict(sentences,
-                                    phrase_prob,
-                                    init_val=init_val)
         correct = {(1, 1): Frac(-1),
                    (2, 2): Frac(-2),
                    # lack one value
@@ -310,6 +579,9 @@ class FutureCostEstimateTest(unittest.TestCase):
                    (7, 9): Frac(-23, 10),
                    (6, 9): Frac(-23, 10),
                    }
+        ans = _create_estimate_dict(sentences,
+                                    phrase_prob,
+                                    init_val=init_val)
         self.assertEqual(ans, correct)
 
 
