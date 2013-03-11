dfef45ab1a29bf3a1e12703922669e0d7a6daab5
Noriyuki Abe <kenko.py@gmail.com>
2013-03-11 13:50:15 -0700

    implement create_ngram_count_except_last_view

--- jec_basic_sentence/jec_createngramdb.py
+++ jec_basic_sentence/jec_createngramdb.py
@@ -4,12 +4,14 @@
 from __future__ import division, print_function
 import keitaiso
 from smt.db.createngramdb import create_ngram_count_db
+from smt.db.createngramdb import create_ngram_count_except_last_view
 
 if __name__ == '__main__':
     db = "sqlite:///:jec_log_basic:"
     lang = 1
     langmethod = keitaiso.str2wakati
     n = 3
-    create_ngram_count_db(lang=lang, langmethod=langmethod,
-                          n=n,
-                          db=db)
+    #create_ngram_count_db(lang=lang, langmethod=langmethod,
+    #                      n=n,
+    #                      db=db)
+    create_ngram_count_except_last_view(lang=lang, db=":jec_log_basic:")
