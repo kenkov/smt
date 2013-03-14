de4560a2b47cfc97d81f9ccac5ba90c95850efd4
Noriyuki Abe <kenko.py@gmail.com>
2013-03-13 22:20:43 -0700

    revise view

--- jec_basic_sentence/jec_createngramdb.py
+++ jec_basic_sentence/jec_createngramdb.py
@@ -3,15 +3,15 @@
 
 from __future__ import division, print_function
 import keitaiso
-from smt.db.createngramdb import create_ngram_count_db
-from smt.db.createngramdb import create_ngram_count_except_last_view
+from smt.db.createngramdb import create_ngram_db
+
 
 if __name__ == '__main__':
     db = "sqlite:///:jec_log_basic:"
     lang = 1
     langmethod = keitaiso.str2wakati
     n = 3
-    #create_ngram_count_db(lang=lang, langmethod=langmethod,
-    #                      n=n,
-    #                      db=db)
-    create_ngram_count_except_last_view(lang=lang, db=":jec_log_basic:")
+    create_ngram_db(lang=lang,
+                    langmethod=langmethod,
+                    n=n,
+                    db=":jec_log_basic:")
--- smt/db/createngramdb.py
+++ smt/db/createngramdb.py
@@ -3,14 +3,16 @@
 
 from __future__ import division, print_function
 import collections
+import sqlite3
 # import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, TEXT, INTEGER
+from sqlalchemy import Column, TEXT, INTEGER, REAL
 from sqlalchemy.orm import sessionmaker
 # smt
 from smt.db.createdb import Sentence
 from smt.langmodel.ngram import ngram
+import math
 
 
 def _create_ngram_count_db(lang, langmethod=lambda x: x,
@@ -50,6 +52,7 @@ def create_ngram_count_db(lang, langmethod=lambda x: x,
         second = Column(TEXT)
         third = Column(TEXT)
         count = Column(INTEGER)
+
     # create table
     Trigram.__table__.drop(engine, checkfirst=True)
     Trigram.__table__.create(engine)
@@ -65,6 +68,115 @@ def create_ngram_count_db(lang, langmethod=lambda x: x,
                        count=count)
         session.add(item)
     session.commit()
+
+
+# create views using SQLite3
+def create_ngram_count_except_last_view(lang, db=":memory:"):
+    # create phrase_count table
+    fromtablename = "lang{}trigram".format(lang)
+    table_name = "lang{}trigram_except_last".format(lang)
+    # create connection
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    try:
+        cur.execute("drop view {0}".format(table_name))
+    except sqlite3.Error:
+        print("{0} view does not exists.\n\
+              => creating a new view".format(table_name))
+    cur.execute("""create view {}
+                as select first, second, sum(count) as count from
+                {} group by first, second order by count
+                desc""".format(table_name, fromtablename))
+    con.commit()
+
+
+def create_ngram_prob(lang,
+                      db=":memory:"):
+
+    # Create connection in sqlite3 to use view
+    table_name = "lang{}trigram_except_last".format(lang)
+    # create connection
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+
+    class Trigram(declarative_base()):
+        __tablename__ = 'lang{}trigram'.format(lang)
+        id = Column(INTEGER, primary_key=True)
+        first = Column(TEXT)
+        second = Column(TEXT)
+        third = Column(TEXT)
+        count = Column(INTEGER)
+
+    class TrigramProb(declarative_base()):
+        __tablename__ = 'lang{}trigramprob'.format(lang)
+        id = Column(INTEGER, primary_key=True)
+        first = Column(TEXT)
+        second = Column(TEXT)
+        third = Column(TEXT)
+        prob = Column(REAL)
+
+    # create connection in SQLAlchemy
+    sqlalchemydb = "sqlite:///{}".format(db)
+    engine = create_engine(sqlalchemydb)
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # create table
+    TrigramProb.__table__.drop(engine, checkfirst=True)
+    TrigramProb.__table__.create(engine)
+
+    # calculate total number
+    query = session.query(Trigram)
+    totalnumber = len(query.all())
+
+    # get trigrams
+    query = session.query(Trigram)
+    for item in query:
+        first, second, third = item.first, item.second, item.third
+        count = item.count
+
+        cur.execute("""select * from {} where \
+                    first=? and\
+                    second=?""".format(table_name),
+                    (first, second))
+        one = cur.fetchone()
+        # if fetch is failed, one is NONE (no exceptions are raised)
+        if not one:
+            print("not found correspont first and second")
+            continue
+        else:
+            alpha = 0.00017
+            c = count
+            n = one[2]
+            v = totalnumber
+            logprob = math.log((c + alpha) / (n + alpha * v))
+            print(u"{}, {}, {}:\
+                  log({} + {} / {} + {} + {}) = {}".format(first,
+                                                           second,
+                                                           third,
+                                                           c,
+                                                           alpha,
+                                                           n,
+                                                           alpha,
+                                                           v,
+                                                           logprob))
+            trigramprob = TrigramProb(first=first,
+                                      second=second,
+                                      third=third,
+                                      prob=logprob)
+            session.add(trigramprob)
+    session.commit()
+
+
+def create_ngram_db(lang, langmethod=lambda x: x,
+                    n=3, db=":memory:"):
+
+    sqlalchemydb = "sqlite:///{}".format(db)
+    create_ngram_count_db(lang=lang, langmethod=langmethod,
+                          n=n,
+                          db=sqlalchemydb)
+    create_ngram_count_except_last_view(lang=lang, db=db)
+    create_ngram_prob(lang=lang, db=db)
 
 
 if __name__ == '__main__':
